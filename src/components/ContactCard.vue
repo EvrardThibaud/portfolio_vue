5d764a1a727920cff58db8ac68eb85968349601d
evti <obit2004@hotmail.com>
2025-01-17 20:16:33 +0100

    started contact card

--- src/components/ContactCard.vue
+++ src/components/ContactCard.vue
@@ -2,8 +2,16 @@
 
 <template>
   <div class="card">
-    <i class="fa-brands fa-linkedin"></i>
-  </div>
+    <a
+      href="https://www.linkedin.com/in/thibaud-evrard-869431258/"
+      target="_blank"
+      class="text-3xl hover:opacity-75"
+    >
+      <i class="fa-brands fa-linkedin"></i>
+    </a>
+        thibaudevrard@outlook.com
+        <i class="fa-solid fa-envelope"></i>
+</div>
 </template>
 
 <style scoped></style>
