9292ec81335477cda6aa040a4d4f31527aeb913d
evti <obit2004@hotmail.com>
2025-01-19 23:59:40 +0100

    github

--- src/components/PresentationCard.vue
+++ src/components/PresentationCard.vue
@@ -8,12 +8,23 @@
       I'm a 20-year-old computer science student with a deep passion for web
       development.
     </p>
-    <a href="cv_thibaud_evrard.pdf" target="_blank">
-      <div class="small_card mt-2">
-        <p>My CV</p>
-        <i class="fa-solid fa-arrow-up-right-from-square"></i>
-      </div>
-    </a>
+    <div class="mt-4 flex flex-wrap gap-2">
+      <a href="cv_thibaud_evrard.pdf" target="_blank">
+        <div class="small_card">
+          <p>My CV</p>
+          <i class="fa-solid fa-arrow-up-right-from-square"></i>
+        </div>
+      </a>
+      <a
+        href="https://github.com/EvrardThibaud"
+        target="_blank"
+        class="text-2xl flex"
+      >
+        <div class="small_card">
+          <i class="fa-brands fa-github"></i>
+        </div>
+      </a>
+    </div>
   </div>
 </template>
 
--- src/composable/cardEffect.js
+++ src/composable/cardEffect.js
@@ -1,30 +1,28 @@
-import { onMounted, onBeforeUnmount } from 'vue';
+import { onMounted, onBeforeUnmount } from "vue";
 
 export function cardEffect(selector) {
   const handleMouseEnter = (event) => {
     const target = event.target;
 
     if (target.matches(selector)) {
-      const rect = target.getBoundingClientRect(); 
-      const mouseX = event.clientX - rect.left; 
-      const mouseY = event.clientY - rect.top;  
+      let rect = target.getBoundingClientRect();
 
       const handleMouseMove = (moveEvent) => {
+        rect = target.getBoundingClientRect();
         const mouseMoveX = moveEvent.clientX - rect.left;
         const mouseMoveY = moveEvent.clientY - rect.top;
 
-         const maxX = rect.width; // Largeur de la carte
-        const rotateY = ((mouseMoveX / maxX) - 0.5) * 10; 
-        const rotateX = ((mouseMoveY / rect.height) - 0.5) * 10; 
+        const maxX = rect.width; // Largeur de la carte
+        const rotateY = (mouseMoveX / maxX - 0.5) * 10;
+        const rotateX = (mouseMoveY / rect.height - 0.5) * 10;
 
-
-        target.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${-rotateY}deg) rotateZ(0deg)`;  
+        target.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${-rotateY}deg) rotateZ(0deg)`;
       };
 
       target.addEventListener("mousemove", handleMouseMove);
 
       target.addEventListener("mouseout", () => {
-        target.style.transform = ""; 
+        target.style.transform = "";
       });
     }
   };
