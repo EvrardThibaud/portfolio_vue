b1a8f243aa15ab6994eea86ca9bf2a0598e01702
evti <obit2004@hotmail.com>
2025-01-22 20:21:12 +0100

    projects page

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,8 +1,15 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import ProjectCard from "../components/ProjectCard.vue";
+
+const projects = ref(JSON.parse(localStorage.getItem("projects")) || []);
+</script>
 
 <template>
-    <h1>Project</h1>
-
+  <h1>Projects</h1>
+  <template v-for="(project, index) in projects" :key="index">
+    <ProjectCard :project="project"></ProjectCard>
+  </template>
 </template>
 
 <style scoped></style>
