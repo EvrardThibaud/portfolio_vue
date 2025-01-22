6a16e13b64906921e7b57a20f2b975a2b5ff13d5
evti <obit2004@hotmail.com>
2025-01-22 23:39:31 +0100

    fix scroll

--- src/router.js
+++ src/router.js
@@ -18,7 +18,7 @@ const routes = [
   },
   ,
   {
-    path: "/:catchAll(.*)", 
+    path: "/:catchAll(.*)",
     name: "Error404",
     component: () => import("./pages/Error404.vue"),
   },
@@ -27,6 +27,13 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition; 
+    } else {
+      return { top: 0 }; 
+    }
+  },
 });
 
 export default router;
