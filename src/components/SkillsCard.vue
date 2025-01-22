3c4d85a9cc83e9fc640079bdbcc9df4e96d6aee8
evti <obit2004@hotmail.com>
2025-01-22 23:55:23 +0100

    viewMore stock in local storage

--- src/components/SkillsCard.vue
+++ src/components/SkillsCard.vue
@@ -1,15 +1,19 @@
 <script setup>
-import { ref } from "vue";
 import Skill from "./Skill.vue";
+import { ref, watch } from "vue";
+
+const skills = ref(JSON.parse(localStorage.getItem("skills")) || []);
 const skillsContainer = ref(null);
-const viewMoreContent = ref("more");
-const skills = ref(JSON.parse(localStorage.getItem('skills')) || []);
+const viewMore = ref(localStorage.getItem("viewMore") || "false");
 
 function handleViewMoreSkills() {
   skillsContainer.value.classList.toggle("h-20");
   skillsContainer.value.classList.toggle("overflow-hidden");
-  viewMoreContent.value = viewMoreContent.value === "more" ? "less" : "more";
 }
+
+watch(viewMore, (newValue) => {
+  localStorage.setItem("viewMore", newValue);
+});
 </script>
 
 <template>
@@ -18,7 +22,10 @@ function handleViewMoreSkills() {
     <div
       id="skillsContainer"
       ref="skillsContainer"
-      class="flex flex-wrap gap-2 py-2 px-1 h-20 overflow-hidden mt-6"
+      :class="[
+        'flex flex-wrap gap-2 py-2 px-1 mt-6',
+        viewMore ? '' : 'h-20 overflow-hidden',
+      ]"
     >
       <div class="flex-1" v-for="(skill, index) in skills" :key="index">
         <Skill :skill="skill"></Skill>
@@ -30,13 +37,13 @@ function handleViewMoreSkills() {
       <p>figma</p>
     </div>
 
-    <p class="hover w-fit" @click="handleViewMoreSkills">
-      View {{ viewMoreContent }}
+    <p class="hover w-fit" @click="(viewMore = !viewMore)">
+      View {{ viewMore ? 'less' : "more" }}
       <i
         :class="
-          viewMoreContent === 'more'
-            ? 'fa-solid fa-angles-down'
-            : 'fa-solid fa-angles-up'
+          viewMore
+          ? 'fa-solid fa-angles-up'
+          : 'fa-solid fa-angles-down'
         "
       ></i>
     </p>
