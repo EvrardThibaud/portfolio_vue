d19dda9fe4a244721eddf579ffe0fd83fbfd2666
evti <obit2004@hotmail.com>
2025-01-25 18:32:51 +0100

    screenshots gallery

--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -6,6 +6,13 @@ import Skill from "../components/Skill.vue";
 const route = useRoute();
 
 const viewMore = ref(false);
+const screenshots = ref(null);
+const showingScreenshot = ref(false);
+
+function handleViewScreenshots() {
+  screenshots.value.classList.toggle("smooth-transition");
+  showingScreenshot.value = !showingScreenshot.value;
+}
 
 const projects = JSON.parse(localStorage.getItem("projects")) || [];
 const project = computed(() => {
@@ -39,13 +46,13 @@ const filteredSkills = computed(() => {
       </p>
     </div>
 
-    <div class="flex flex-wrap gap-2 py-2 px-1 my-3">
+    <div v-if="project.skills" class="flex flex-wrap gap-2 py-2 px-1 my-3">
       <template v-for="(skill, index) in filteredSkills" :key="index">
         <Skill :skill="skill"></Skill>
       </template>
     </div>
 
-    <div class="flex gap-2 flex-wrap">
+    <div v-if="project.github" class="flex gap-2 flex-wrap">
       <template v-for="(link, index) in project.github" :key="index">
         <div class="button">
           <a :href="link" target="_blank" class="flex items-center gap-2">
@@ -64,25 +71,59 @@ const filteredSkills = computed(() => {
     >
       Unfortunately, the source code of this project is not available.
     </p>
+
     <template v-if="project.src.length > 0">
-      <p class="text-xl font-semibold">Project Screenshots</p>
+      <p class="text-xl font-semibold my-3">Project Screenshots</p>
+      <div class="button" ref="screenshots" @click="handleViewScreenshots">
+        <template
+          v-for="(src, index) in showingScreenshot
+            ? project.src
+            : project.src.slice(0, 3)"
+          :key="index"
+        >
+          <img
+            :src="src"
+            :alt="project.name + ' ' + index"
+            class="size-10 rounded"
+          />
+        </template>
+        <p v-if="project.src.length > 3 && !showingScreenshot">
+          + {{ project.src.length - 3 }}
+        </p>
+      </div>
     </template>
+
     <template v-else
       ><p class="opacity-75 text-center my-3">
         Unfortunately, there are no screenshots for this project.
       </p></template
     >
-    <template v-for="(src, index) in project.src" :key="index">
-      <img
-        :src="src"
-        :alt="project.name + ' ' + index"
-        class="w-full my-2 rounded"
-      />
-    </template>
   </div>
 </template>
 
 <style scoped>
+.smooth-transition {
+  position: absolute;
+  left: 50%; 
+  top: 50%;
+  z-index: 10;
+  width: 70%;
+  overflow-x: auto;
+  transform: translate(-50%, -50%);
+  scroll-snap-type: x mandatory;  
+  scroll-behavior: smooth; 
+
+  &:hover{
+    cursor: default;
+  }
+
+  & > img {
+    height: 70vh;
+    scroll-snap-align: center;
+    width: auto;
+  }
+}
+
 .project_description {
   display: -webkit-box;
   -webkit-line-clamp: 3;
