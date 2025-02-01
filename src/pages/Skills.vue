c507a3890e454f69fc063989e5ef5906ea1cc671
evti <obit2004@hotmail.com>
2025-02-01 17:02:02 +0100

    search skills

--- src/pages/Skills.vue
+++ src/pages/Skills.vue
@@ -2,26 +2,43 @@
 import Skill from "../components/Skill.vue";
 import { ref, watch } from "vue";
 
-
+const prompt = ref("");
 
 const skills = ref(JSON.parse(localStorage.getItem("skills")) || []);
 
+const skillsSearched = ref(skills.value);
+
+function handleSearchSkills() {
+  const searchTerm = prompt.value.trim().toLowerCase();
+
+  skillsSearched.value = searchTerm
+    ? skills.value.filter((skill) =>
+        skill.name.toLowerCase().includes(searchTerm)
+      )
+    : skills.value;
+}
 </script>
 
 <template>
   <div class="container">
     <p class="text-3xl font-semibold">My Skills</p>
-    <input type="text" placeholder="Search for a skills">
-    <div class="flex flex-wrap gap-2 my-4 ">
-      <template v-for="(skill, index) in skills" :key="index">
-        <Skill
-          class="flex-1"
-          :skill="skill"
-          @click="handleOpenSkill(index)"
-        ></Skill>
-      </template>
+    <input
+      type="text"
+      placeholder="Search for a skills"
+      v-model="prompt"
+      @input="handleSearchSkills"
+    />
+    <div class="flex flex-wrap gap-2 my-4">
+        <template v-for="(skill, index) in skillsSearched" :key="skill.id">
+          <Skill
+            class="flex-1"
+            :skill="skill"
+            @click="handleOpenSkill(index)"
+          ></Skill>
+        </template>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
