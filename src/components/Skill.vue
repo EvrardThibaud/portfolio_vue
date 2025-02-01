131ab050ea4226df51e7bba8ad76564193bd161d
evti <obit2004@hotmail.com>
2025-02-01 12:55:55 +0100

    minor changes

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -7,7 +7,7 @@ const props = defineProps({
 </script>
 
 <template>
-  <div>
+  <div class="box">
     <img :src="skill.src" :alt="skill.alt" />
     <p class="font-semibold text-nowrap select-none">{{ skill.name }}</p>
   </div>
@@ -15,20 +15,15 @@ const props = defineProps({
 
 <style scoped>
 div {
-  display: flex;
-  align-items: center;
-  justify-content: center;
   gap: 1rem;
-  width: auto;
   padding: 0.2rem 0.8rem;
-  box-shadow: 0px 0px 1px #ffffff9b;
-  border-radius: 3px;
   background-color: #ffffff0d;
 }
 
-img {
+img{
   max-height: 20px;
   max-width: 20px;
   /* filter: brightness(1) saturate(0) invert(1); */
 }
+
 </style>
