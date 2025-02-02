406df12ef1f73ad6048e95ff5bb45385cc3a834a
evti <obit2004@hotmail.com>
2025-02-02 19:07:09 +0100

    view all skills

--- src/components/SkillsCard.vue
+++ src/components/SkillsCard.vue
@@ -5,53 +5,42 @@ import { onMounted } from "vue";
 
 const skills = ref(JSON.parse(localStorage.getItem("skills")) || []);
 const skillsContainer = ref(null);
-const viewMore = ref(null);
-
-watch(viewMore, (newValue) => {
-  localStorage.setItem("viewMore", newValue);
-});
-
-onMounted(() => {
-  viewMore.value = JSON.parse(localStorage.getItem("viewMore")) || false;
-});
-
-function handleViewMoreSkills() {
-  viewMore.value = !viewMore.value;
-  setTimeout(() => {
-    const skillsCard = document.getElementById("skills_card");
-    console.log(skillsCard);
-    if (skillsCard) {
-      skillsCard.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-  }, 100); 
-}
 </script>
 
 <template>
-  <div class="card" id="skills_card">
-    <p class="text-2xl font-semibold leading-7">My Skills</p>
-    <div
-      id="skillsContainer"
-      ref="skillsContainer"
-      :class="[
-        'flex flex-wrap gap-2 py-2 px-1 mt-6',
-        viewMore ? '' : 'h-20 overflow-hidden',
-      ]"
-    >
-      <template v-for="(skill, index) in skills" :key="index">
-        <Skill class="flex-1" :skill="skill"></Skill>
-      </template>
+  <RouterLink :to="{ name: 'skills' }">
+    <div class="card">
+      <div class="flex justify-between items-center">
+        <p class="text-2xl font-semibold leading-7">My Skills</p>
+        <p class="hover">
+          View all
+          <i class="fa-solid fa-arrow-right ml-2"></i>
+        </p>
+      </div>
+      <div
+        id="skillsContainer"
+        ref="skillsContainer"
+        :class="['flex flex-wrap gap-2 py-2 px-1 mt-6']"
+      >
+        <template v-for="(skill, index) in skills.slice(0, 6)" :key="index">
+          <Skill class="flex-1" :skill="skill"></Skill>
+        </template>
+      </div>
     </div>
-
-    <p class="hover w-fit" @click="handleViewMoreSkills">
-      View {{ viewMore ? "Less" : "More" }}
-      <i
-        :class="viewMore ? 'fa-solid fa-angles-up' : 'fa-solid fa-angles-down'"
-      ></i>
-    </p>
-  </div>
+  </RouterLink>
 </template>
 
-<style scoped></style>
+<style scoped>
+.card {
+
+  & > div > p > i{ 
+    transition: all ease-out 0.3s;
+  }
+
+  &:hover {
+    & > div > p > i {
+      transform: translateX(0.5rem);
+    }
+  }
+}
+</style>
