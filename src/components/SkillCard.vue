a095d2ba2203d9337cbb0f4136077f2a0b186898
evti <obit2004@hotmail.com>
2025-02-03 22:55:56 +0100

    router link to project from skills

--- src/components/SkillCard.vue
+++ src/components/SkillCard.vue
@@ -8,14 +8,27 @@ const props = defineProps({
 </script>
 
 <template>
-    <p>{{ skill.name }}</p>
-    <template v-for="(project, index) in projects" :key="index">
-        <template v-if="project.skills.includes(skill.name) ">
-
-            <p>{{project.name}}</p>
-            <img :src="project.src[0]" alt="">
-        </template>
+  <p>{{ skill.name }}</p>
+  <template v-for="(project, index) in projects" :key="index">
+    <template v-if="project.skills.includes(skill.name)">
+      <RouterLink
+        :to="{
+          name: 'project',
+          params: {
+            id: project.id,
+          },
+        }"
+        :style="{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${project.src[0]})`,
+        }"
+      >
+        <div>
+          <p>{{ project.name }}</p>
+          <img :src="project.src[0]" alt="" />
+        </div>
+      </RouterLink>
     </template>
+  </template>
 </template>
 
 <style scoped></style>
