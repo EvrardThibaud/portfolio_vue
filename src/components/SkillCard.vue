1239783304c6577998c8daa856b61a5e0da1488e
evti <obit2004@hotmail.com>
2025-02-04 20:55:53 +0100

    skill card

--- src/components/SkillCard.vue
+++ src/components/SkillCard.vue
@@ -8,27 +8,41 @@ const props = defineProps({
 </script>
 
 <template>
-  <p>{{ skill.name }}</p>
-  <template v-for="(project, index) in projects" :key="index">
-    <template v-if="project.skills.includes(skill.name)">
-      <RouterLink
-        :to="{
-          name: 'project',
-          params: {
-            id: project.id,
-          },
-        }"
-        :style="{
-          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${project.src[0]})`,
-        }"
-      >
-        <div>
-          <p>{{ project.name }}</p>
-          <img :src="project.src[0]" alt="" />
-        </div>
-      </RouterLink>
+  <div class="skill_card card">
+    <p>{{ skill.name }}</p>
+    <template v-for="(project, index) in projects" :key="index">
+      <template v-if="project.skills.includes(skill.name)">
+        <RouterLink
+          :to="{
+            name: 'project',
+            params: {
+              id: project.id,
+            },
+          }"
+          :style="{
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${project.src[0]})`,
+          }"
+        >
+          <div class="project_link">
+            <p>{{ project.name }}</p>
+            <img :src="project.src[0]" alt="" />
+          </div>
+        </RouterLink>
+      </template>
     </template>
-  </template>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.skill_card{
+    
+}
+
+.project_link {
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 20px;
+  width: 90%;
+}
+</style>
