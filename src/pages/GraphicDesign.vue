00db044f726ed70023e01e104833886066b1dc11
evti <obit2004@hotmail.com>
2025-02-07 23:08:21 +0100

    photoshop logo

--- src/pages/GraphicDesign.vue
+++ src/pages/GraphicDesign.vue
@@ -2,6 +2,18 @@
 
 <template>
   <p>Graphic Design</p>
+  <img src="../../public/photoshop_logo.png" alt="" />
 </template>
 
-<style scoped></style>
+<style scoped>
+img {
+    height: 50px;
+    width: 50px;
+  /* -webkit-box-shadow: 0px 0px 10px 10px #001833;
+  -moz-box-shadow: 0px 0px 10px 10px #001833; */
+  box-shadow: 0px 0px 1px white;
+  border-radius: 10px;
+  /* border: 1px white solid; */
+  margin-top: 10px;
+}
+</style>
