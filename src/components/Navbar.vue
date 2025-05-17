2f563a6a1708653b925186a4883374193c3a081d
Thibaud <obit2004@hotmail.com>
2025-05-17 16:49:02 +0200

    Menu item translate on hover

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,22 @@
 <script setup></script>
 
 <template>
-  <nav class="flex items-center fixed h-full w-auto left-0 top-0 p-16 ">
-    <div class="flex flex-col gap-2 ">
+  <nav class="flex items-center fixed h-full w-auto left-0 top-0 p-16">
+    <div class="flex flex-col gap-2">
       <RouterLink :to="{ name: 'home' }" class="hover"> Home </RouterLink>
-      <RouterLink :to="{ name: 'projects' }" class="hover"> Projects </RouterLink>
+      <RouterLink :to="{ name: 'projects' }" class="hover">
+        Projects
+      </RouterLink>
       <RouterLink :to="{ name: 'skills' }" class="hover"> Skills </RouterLink>
-      <RouterLink :to="{ name: 'design' }" class="hover"> Graphic Design </RouterLink>
+      <RouterLink :to="{ name: 'design' }" class="hover">
+        Graphic Design
+      </RouterLink>
     </div>
   </nav>
 </template>
 
-<style scoped></style>
+<style scoped>
+.hover:hover {
+  transform: translateX(0.4rem);
+}
+</style>
