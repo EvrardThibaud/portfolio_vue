37190739f23625a2fd847730e335ddce163283ea
Thibaud <obit2004@hotmail.com>
2025-05-22 10:31:41 +0200

    Data youtube channel

--- src/pages/GraphicDesign.vue
+++ src/pages/GraphicDesign.vue
@@ -66,42 +66,5 @@ fetch(
 </template>
 
 <style scoped>
-#photoshop_logo {
-  height: 50px;
-  width: 50px;
-  /* -webkit-box-shadow: 0px 0px 10px 10px #001833;
-  -moz-box-shadow: 0px 0px 10px 10px #001833; */
-  box-shadow: 0px 0px 1px white;
-  border-radius: 10px;
-  /* border: 1px white solid; */
-  margin-top: 10px;
-}
 
-#youtube_card {
-  background: linear-gradient(
-    145deg,
-    hsla(0, 100%, 50%, 0.5),
-    hsla(0, 100%, 50%, 0.3)
-  );
-  margin: 1rem 0;
-  transition: all 0.5s ease-out;
-  cursor: pointer;
-
-  img {
-    height: 20px;
-  }
-
-  p {
-    white-space: nowrap;
-    font-weight: 500;
-  }
-
-  &:hover {
-    background: linear-gradient(
-      145deg,
-      hsla(0, 100%, 50%, 0.5),
-      hsla(0, 100%, 50%, 0.35)
-    );
-  }
-}
 </style>
