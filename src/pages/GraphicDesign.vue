7db0a7580647b318febeee8b5dd1fee8cf5e8e81
Thibaud <obit2004@hotmail.com>
2025-05-22 11:03:23 +0200

    Save fetched data in local storage

--- src/pages/GraphicDesign.vue
+++ src/pages/GraphicDesign.vue
@@ -1,45 +1,66 @@
 <script setup>
 import { ref } from "vue";
-import Skill from "../components/Skill.vue";
 
-const skills = ref(JSON.parse(localStorage.getItem("skills")) || []);
-const photoshop = skills.value.find((skill) => skill.id === 39);
+const youtubeID = ref(JSON.parse(localStorage.getItem("youtubeID")) || []);
+const youtubeFetchedData = ref([]);
 
-const youtubeurs = ref([]);
-
+const LOCAL_KEY = "youtubeFetchedData";
+const TIMESTAMP_KEY = "youtubeFetchedDataTimestamp";
 const API_KEY = "AIzaSyCM5oAgZbKktibPLUYzy9jgmC2LLvDi8bY";
 
-let channelId = "UC1q7LRamyojZuAbhsBWWgAA";
+const timestamp = localStorage.getItem(TIMESTAMP_KEY);
+const now = new Date().getTime();
+const lastFetchDate = ref("Never");
+if (timestamp) {
+  lastFetchDate.value = new Date(parseInt(timestamp)).toLocaleString();
+}
 
-// get id  `https://www.googleapis.com/youtube/v3/search?part=snippet&type=channel&q=Ashtax&key=${API_KEY}`
- 
+if (
+  !localStorage.getItem(LOCAL_KEY) ||
+  !timestamp ||
+  now - parseInt(timestamp) > 30 * 60 * 1000 
+) {
+  fetchYouTubeDataSequentially().then(() => {
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(youtubeFetchedData.value));
+    localStorage.setItem(TIMESTAMP_KEY, now.toString());
+  });
+} else {
+  youtubeFetchedData.value = JSON.parse(localStorage.getItem(LOCAL_KEY));
+}
+async function fetchYouTubeDataSequentially() {
+  youtubeFetchedData.value = [];
 
-fetch(
-      `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics,brandingSettings&id=${channelId}&key=${API_KEY}`
-    )
-  .then((res) => res.json())
-  .then((data) => {
-    const channel = data.items[0];
-    youtubeurs.value.push({
-      name: channel.snippet.title,
-      subscriberCount: channel.statistics.subscriberCount,
-      img: channel.snippet.thumbnails.default.url,
-      videos: [],
-    });
+  for (const youtubeur of youtubeID.value) {
+    try {
+      const res = await fetch(
+        `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics,brandingSettings&id=${youtubeur.id}&key=${API_KEY}`
+      );
+      const data = await res.json();
+      const channel = data.items[0];
 
-    fetch(
-      `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=LUH58bF86rE&key=${API_KEY}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        youtubeurs.value[0].videos.push({
-          title: data.items[0].snippet.title,
-          thumbnail: data.items[0].snippet.thumbnails.maxres.url,
-          viewCount: data.items[0].statistics.viewCount,
-        });
+      youtubeFetchedData.value.push({
+        name: channel.snippet.title,
+        subscriberCount: channel.statistics.subscriberCount,
+        img: channel.snippet.thumbnails.default.url,
+        videos: [],
       });
-  })
-  .catch((err) => console.error("Erreur :", err));
+    } catch (err) {
+      console.error("Erreur :", err);
+    }
+  }
+}
+ 
+// fetch(
+      //   `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=LUH58bF86rE&key=${API_KEY}`
+      // )
+      //   .then((res) => res.json())
+      //   .then((data) => {
+      //     youtubeurs.value[0].videos.push({
+      //       title: data.items[0].snippet.title,
+      //       thumbnail: data.items[0].snippet.thumbnails.maxres.url,
+      //       viewCount: data.items[0].statistics.viewCount,
+      //     });
+      //   });
 
 </script>
 
@@ -47,9 +68,9 @@ fetch(
   <div class="container">
     <h1 class="text-3xl font-semibold">Projects</h1>
 
-    <Skill :skill="photoshop"></Skill>
+    <p>Last update : {{ lastFetchDate }}</p>
 
-    <template v-for="youtubeur in youtubeurs">
+    <template v-for="youtubeur in youtubeFetchedData">
       <div>
         <img :src="youtubeur.img" :alt="youtubeur.name + ' profile picture'" />
         <h2>{{ youtubeur.name }}</h2>
