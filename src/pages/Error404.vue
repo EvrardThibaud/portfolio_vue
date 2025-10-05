347284e75184697dbac78ca92c4e0d15b07d109f
Thibaud <obit2004@hotmail.com>
2025-10-05 15:05:55 +0200

    Error 404

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,7 +1,28 @@
-<script setup lang="ts"></script>
+<script setup>
+import FuzzyText from "../components/ui/FuzzyText.vue";
+</script>
 
 <template>
-  <p>Error 404</p>
+  <div class="h-screen w-screen flex flex-col items-center justify-center gap-2">
+    <FuzzyText
+      text="error"
+      :font-size="20"
+      font-weight="900"
+      color="#fff"
+      :enable-hover="true"
+      :base-intensity="0.0"
+      :hover-intensity="0.5"
+    />
+    <FuzzyText
+      text="404"
+      :font-size="140"
+      font-weight="900"
+      color="#fff"
+      :enable-hover="true"
+      :base-intensity="0.18"
+      :hover-intensity="0.5"
+    />
+  </div>
 </template>
 
 <style scoped></style>
