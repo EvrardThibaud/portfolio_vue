6ef34311f8c8fc965bdd47a9c8b412fad8fb31d6
Thibaud <obit2004@hotmail.com>
2025-10-06 19:56:45 +0200

    ClickSpark

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
+import ClickSpark from "./components/ui/ClickSpark.vue";
 </script>
 
 <template>
-  <!-- <Navbar></Navbar> -->
-  <main class="flex flex-col items-center">
-    <RouterView></RouterView>
-  </main>
-  <Footer></Footer>
+  <ClickSpark
+    spark-color="#81a9b8"
+    :spark-size="12"
+    :spark-radius="25"
+    :spark-count="9"
+    :duration="600"
+    easing="ease-out"
+    :extra-scale="1.2"
+    class="interactive-area"
+  >
+    <!-- <Navbar></Navbar> -->
+    <main class="flex flex-col items-center">
+      <RouterView></RouterView>
+    </main>
+    <Footer></Footer>
+  </ClickSpark>
 </template>
 
 <style scoped>
+.interactive-area {
+  width: 100vw;
+  height: auto;
+}
 </style>
