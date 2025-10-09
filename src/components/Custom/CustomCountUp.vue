65729e01d7483a1cfa8f7e9a0f88b38550ca7195
Thibaud <obit2004@hotmail.com>
2025-10-09 20:51:37 +0200

    CustomCountUp

---
+++ src/components/Custom/CustomCountUp.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import CountUp from '@/components/ui/CountUp.vue'
+
+const props = defineProps({
+    title: String,
+    to: { type: Number, required: true },
+    suffix: String,
+})
+</script>
+
+<template>
+    <div class="countup-container">
+        <p>{{ title }}</p>
+        <div class="countup">
+            <CountUp :to="to" :delay="0" :duration="1" :start-when="true" />
+            <p>{{ suffix }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.countup-container {
+    text-align: center;
+    padding: 50px;
+    border-radius: 8px;
+    box-shadow: 0px 0px 1px #81a9b8;
+    background: linear-gradient(145deg, #121212, #0f0f0f);
+
+    .countup {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-weight: bold;
+        font-size: 3rem;
+        color: #81a9b8;
+    }
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
-import CustomCarousel from "@/components/CustomCarousel.vue";
+import CustomCarousel from "@/components/Custom/CustomCarousel.vue";
+import CustomCountUp from "@/components/Custom/CustomCountUp.vue";
 </script>
 
 <template>
   <main>
     <CustomCarousel></CustomCarousel>
+    <div class="flex flex-col gap-11">
+      <CustomCountUp
+        :title="'YouTube Thumbnails I’ve Designed'"
+        :to="1000"
+        :suffix="'+'"
+      ></CustomCountUp>
+      <CustomCountUp :title="'Client'" :to="20" :suffix="'+'"></CustomCountUp>
+    </div>
   </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
